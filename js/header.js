b8c625f4a69d21f6b5b90128370df010767677e5
Sadgman <angel.luis.regalado@hotmail.com>
2025-08-28 18:00:32 -0400

    Add header navigation and logo to header.js

---
+++ js/header.js
@@ -0,0 +1,19 @@
+document.write(`
+      <div class="logo">
+        <a href="index.html"><img name="logo-farquina.png" alt="logo Farmoquímica" width="300"/></a>
+      </div>
+      <ul class="nav-links">
+        <li><a href="index.html">Hogar</a></li>
+        <li><a href="resource/sobre.html">Sobre nosotros</a></li>
+        <li class="dropdown">
+          <a href="resource/productos.html">Productos ▾</a>
+          <ul class="dropdown-menu">
+            <li><a href="resource/Limpieza.html">Limpieza del Hogar</a></li>
+            <li><a href="resource/aseo.html">Aseo Personal</a></li>
+            <li><a href="resource/vehiculo.html">Cuidado de Vehículos</a></li>
+            <li><a href="resource/comestibles.html">Comestibles</a></li>
+          </ul>
+        </li>
+        <li><a href="resource/contactos.html">Contactos</a></li>
+      </ul>
+    `)

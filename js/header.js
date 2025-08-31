700851bf498ffadcedb18bf23da3afac5ca780ea
Sadgman <angel.luis.regalado@hotmail.com>
2025-08-31 12:27:44 -0400

    Actualizar estructura del encabezado y rutas de los enlaces para consistencia

--- js/header.js
+++ js/header.js
@@ -4,18 +4,18 @@ document.addEventListener('DOMContentLoaded', function() {
       <a href="index.html"><img name="logo-farquina.png" alt="logo Farmoquímica" width="300"/></a>
     </div>
     <ul class="nav-links">
-      <li><a href="index.html">Hogar</a></li>
-      <li><a href="resource/sobre.html">Sobre nosotros</a></li>
+      <li><a href="../index.html">Hogar</a></li>
+      <li><a href="sobre.html">Sobre nosotros</a></li>
       <li class="dropdown">
-        <a href="resource/productos.html">Productos ▾</a>
+        <a href="productos.html">Productos ▾</a>
         <ul class="dropdown-menu">
-          <li><a href="resource/Limpieza.html">Limpieza del Hogar</a></li>
-          <li><a href="resource/aseo.html">Aseo Personal</a></li>
-          <li><a href="resource/vehiculo.html">Cuidado de Vehículos</a></li>
-          <li><a href="resource/comestibles.html">Comestibles</a></li>
+          <li><a href="Limpieza.html">Limpieza del Hogar</a></li>
+          <li><a href="aseo.html">Aseo Personal</a></li>
+          <li><a href="vehiculo.html">Cuidado de Vehículos</a></li>
+          <li><a href="comestibles.html">Comestibles</a></li>
         </ul>
       </li>
-      <li><a href="resource/contactos.html">Contactos</a></li>
+      <li><a href="contactos.html">Contactos</a></li>
     </ul>
   `;
 });

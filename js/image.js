9149395b303e7e67da8c582e32bacb5c4a486a03
Sadgman <angel.luis.regalado@hotmail.com>
2025-09-02 06:40:21 -0400

    Añadir menú lateral y mejorar la responsividad del diseño

--- js/image.js
+++ js/image.js
@@ -3,3 +3,21 @@ document.addEventListener('DOMContentLoaded', function() {
         img.src = 'https://alfican.github.io/Pag-web_s10/image/' + img.getAttribute('name')
     })
 })
+document.addEventListener('DOMContentLoaded', function() {
+  const menuBtn = document.getElementById('menu-btn');
+  const navLateral = document.querySelector('.nav-lateral');
+
+
+  if (menuBtn && navLateral) {
+    menuBtn.addEventListener('click', (e) => {
+      navLateral.classList.toggle('open');
+      e.stopPropagation();
+    });
+  }
+  document.addEventListener('click', e => {
+    if(!menuBtn.contains(e.target) && !navLateral.contains(e.target)){
+        navLateral.classList.remove('open');
+        this.body.classList.remove('open');
+    }
+  })
+});

435add0d4744759f6442ed361fdafacf9f53eb6c
Mr_Red <70178924+Sadgman@users.noreply.github.com>
2025-09-04 08:55:47 -0400

    Update paths

--- js/header.js
+++ js/header.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+  document.URL.replace()
   document.querySelector('#nav-bar').innerHTML = `
   <a href="index.html" id="logo"><img name="logo-farquina.png" alt="logo Farmoquímica" width="300"/></a>
 
@@ -10,19 +11,20 @@ document.addEventListener('DOMContentLoaded', function() {
   <nav class="nav-lateral">
     <ul class="nav-links">
       <li><a href="../index.html">Hogar</a></li>
-      <li><a href="resource/sobre.html">Sobre nosotros</a></li>
+      <li><a href="sobre.html">Sobre nosotros</a></li>
       <li class="dropdown">
         <a href="#">Productos ▾</a>
         <ul class="dropdown-menu">
-          <li><a href="../Limpieza.html">Limpieza del Hogar</a></li>
-          <li><a href="../aseo.html">Aseo Personal</a></li>
-          <li><a href="../vehiculo.html">Cuidado de Vehículos</a></li>
-          <li><a href="../comestibles.html">Comestibles</a></li>
+          <li><a href="Limpieza.html">Limpieza del Hogar</a></li>
+          <li><a href="aseo.html">Aseo Personal</a></li>
+          <li><a href="vehiculo.html">Cuidado de Vehículos</a></li>
+          <li><a href="comestibles.html">Comestibles</a></li>
           <!-- resource/productos.html -->
         </ul>
       </li>
-      <li><a href="../contactos.html">Contactos</a></li>
+      <li><a href="contactos.html">Contactos</a></li>
     </ul>
   </nav>
   `;
+
 });
